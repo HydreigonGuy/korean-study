42f5aa0668a4487a4dc4badda4c88363a5255fc4
HydreigonGuy <ad.thibault18@gmail.com>
2024-07-22 00:25:59 +0200

    added a biginning of a file reader

---
+++ src/file_loader.py
@@ -0,0 +1,25 @@
+
+import os
+import json
+
+FILES_PATH = "../vocab"
+
+def retrieve_file_contents(path):
+    files = os.listdir(path)
+    contents = {}
+
+    for f in files:
+        if os.path.isdir(os.path.join(path, f)):
+            data = retrieve_file_contents(os.path.join(path, f))
+            for d in data:
+                contents[d] = data[d]
+        elif os.path.isfile(os.path.join(path, f)):
+            file = open(os.path.join(path, f))
+            data = json.loads(file.read())
+            file.close()
+            for d in data:
+                contents[d] = data[d]
+    return (contents)
+
+contents = retrieve_file_contents(FILES_PATH)
+print(contents)

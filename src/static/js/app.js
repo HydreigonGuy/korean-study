a616723dd0a9a934d60c159b1cd91a696e2d6631
HydreigonGuy <ad.thibault18@gmail.com>
2024-08-14 12:26:31 +0200

    added checking of words and displays previous words with the solution

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -14,5 +14,24 @@ function get_word() {
 function check() {
     const input = document.getElementById("input").value;
 
-    console.log(input);
+    var word_info = "";
+    var correct = false;
+    var kr_list = []
+    for (let k_word in word.korean) {
+        if (word.korean[k_word].word == input)
+            correct = true;
+        kr_list.push(word.korean[k_word].word)
+    }
+    if (correct)
+        word_info = "O"
+    else
+        word_info = "X"
+    word_info = word_info + " - " + word.word + ": " + kr_list.join(', ') + "<br/>";
+    document.getElementById("previous-words").innerHTML = word_info + document.getElementById("previous-words").innerHTML;
+    document.getElementById("input").value = "";
+    get_word();
+}
+
+window.onload = function(e){
+    get_word();
 }

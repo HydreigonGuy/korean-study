48b831c0ed52b1addf038e13ff0052d6d676adb0
HydreigonGuy <ad.thibault18@gmail.com>
2024-10-09 15:50:36 +0200

    added history tracking

--- src/models/views.py
+++ src/models/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 import json
 import random
-from models.models import EnglishWord, KoreanWord, Profile, Accuracy
+from models.models import EnglishWord, KoreanWord, Profile, Accuracy, History
 
 def index(request):
   if request.user.is_authenticated:
@@ -15,12 +15,16 @@ def index(request):
 
 def get_word(request):
   if request.user.is_authenticated:
+    # For logged in User
     prev_word = request.GET.get('word', '')
     prev_res = request.GET.get('res', '')
     if (prev_word != '' or prev_res != ''):
+      # Log previous word
       try:
         previous_word = EnglishWord.objects.get(word=prev_word)
         profile = Profile.objects.get(user=request.user)
+
+        # Log accuracy
         if (Accuracy.objects.filter(word=previous_word, profile=profile).exists()):
           acc = Accuracy.objects.get(word=previous_word, profile=profile)
           acc.total_guesses = acc.total_guesses + 1
@@ -33,8 +37,20 @@ def get_word(request):
             correct_guesses = 1
           new_acc = Accuracy(word=previous_word, profile=profile, correct_guesses=correct_guesses, total_guesses=1)
           new_acc.save()
+        
+        # Log history
+        max_history = 5
+        new_history = History(word=previous_word, success=False, Profil=profile)
+        if (prev_res == 'true'):
+          new_history.success = True
+        new_history.save()
+        while (len(History.objects.filter(Profil=profile)) > max_history):
+          History.objects.filter(Profil=profile).order_by('datetime')[0].delete()
+
       except:
         pass
+
+    # Get next word
     items = list(EnglishWord.objects.all())
     random_eng = random.choice(items)
 
@@ -46,7 +62,9 @@ def get_word(request):
       "korean":korean_words
     }
     return HttpResponse(json.dumps(data), content_type='application/json')
+
   else:
+    # For anonymous user
     items = list(EnglishWord.objects.all())
     random_eng = random.choice(items)
     korean_words = []
